b8e183b1c6cec805833401c850dc75f4ecbffbc4
mallesha <mallesha666@gmail.com>
2020-04-04 12:00:04 +0200

    hostname invalid parameter handling added

---
+++ mallesh-test/neg-service-create.py
@@ -0,0 +1,43 @@
+import  json
+
+device = "T02-ntsm-ci-ios38"
+hostname = "localhost"
+banner = "test123-banner"
+x = {
+  "hname-banner-rfs:hname-banner-rfs": [
+    {
+      "device": device,
+      "hostname": hostname,
+      "login-banner": banner
+    }
+  ]
+}
+
+#print(json.dumps(x))
+#print(x['hname-banner-rfs:hname-banner-rfs'][0]['device'])
+
+x['hname-banner-rfs:hname-banner-rfs'][0]['device'] = hostname
+
+data=x['hname-banner-rfs:hname-banner-rfs'][0]['device']
+
+''' The below if block ceck the invalid entry under the hostname parameter
+Examples:
+hostname emty
+hostname too long more than 32 char length
+hostname startes with -
+hostname ends with -
+hostname is localhost
+hostname char range
+'''
+if not data:
+  print("hostname format not supported")
+
+elif ((data[0] == "-")
+        or (data[-1] == "-")
+        or (data == "localhost")
+        or (len(data) > 32)
+        ):
+  print("hostname format not supported")
+
+#print(x['hname-banner-rfs:hname-banner-rfs'][0]['device'])
+#print (json.dumps(x))
